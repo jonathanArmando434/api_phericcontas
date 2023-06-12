9dba85b5bf3fb90a9589dae64d78c7ad93bade58
luiscbaptista <luiscbaptista0@gmail.com>
2023-06-12 21:18:56 +0100

    Login

--- models/LocalizacaoModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const LocalizacaoSchema = new mongoose.Schema({
-    endereco: { type: String, require: true },
-    isPrincipal: { type: Boolean, require: true },
-});
-
-module.exports = LocalizacaoSchema;
-
--- routes/LoginRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router() 
-const { loginMiddleware } = require('../middlewares/loginMiddleware')
-
-router.post('/', loginMiddleware)
-
-module.exports = router
